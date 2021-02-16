ee61ae7d987dc2f05f84d2bdfdf3a3db03b099c2
BurningNacho <33518443+BurningNacho@users.noreply.github.com>
2021-02-16 16:50:53 +0100

    adding new module Creates a task if a location is defined a work

---
+++ create_task_for_works/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Añade una tarea nueva relacionada con la obra creada",
+
+    'summary': """""",
+
+    'description': """
+        Añade una tarea nueva relacionada con la obra creada
+            En las facturas con proveedor podemos
+            seleccionar los movimientos asociados.
+    """,
+
+    'author': "Nacho",
+    'website': "http://www.ignacioragamiguel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock', 'project'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'templates.xml',
+        'views/create_task_or_work_form.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo.xml',
+    ],
+}
---
+++ create_task_for_works/extndmodel.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class Move(models.Model):
+    _inherit = 'stock.warehouse'
+
+
+    x_invoice_id = fields.Many2one('project.project',
+        string="Factura de referencia", ondelete='set null')
+
+
+class Invoice(models.Model):
+    _inherit = 'project.project'
+
+    related_stock_moves = fields.One2many('stock.warehouse',
+        'x_invoice_id', string="Movimiento asociado")
+
+
+    # @api.multi
+    # def action_add_lines(self):
+    #     invoice_line_obj = self.env['account.invoice.line']
+    #     for each in self:
+    #         for move in each.related_stock_moves:
+    #             rec = {
+    #                 'account_id': each.account_id.id,
+    #                 'invoice_id': each.id,
+    #                 'product_id': move.product_id.id,
+    #                 'quantity': move.product_uom_qty,
+    #                 'name': move.product_id.name,
+    #                 'price_unit': '0.0'
+    #             }
+    #             if move.product_id.description: rec['name'] = move.product_id.description
+    #             invoice_line_obj.create(rec)
+    #     return True
+
+
+
+
+
+
+
+#     invoice_line_ids = fields.One2many('account.invoice.line',
+#         'invoice_id', string="Líneas de la factura", compute='_add_lines')
+#
+# ## Retrieve product_id and product_uom_qty
+#     @api.depends('related_stock_moves')
+#     @api.one
+#     def _add_lines(self):
+#         for move in self.related_stock_moves:
+#             self.env['account.invoice.line'].create({
+#                     'account_id': self.account_id.id,
+#                     'invoice_id': self.id,
+#                     'product_id': move.product_id.id,
+#                     'quantity': move.product_uom_qty,
+#                     'name': move.product_id.description,
+#                     'price_unit': '0.0'
+#             })
